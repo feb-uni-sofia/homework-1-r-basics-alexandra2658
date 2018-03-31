543a77f3b4cdf4cd54d6140f32cfb02af4a9a53f
Alexandra Todorova <37662635+alexandra2658@users.noreply.github.com>
2018-03-31 11:51:55 +0300

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,20 @@
+#Problem one
+#1.Create a vector with the numbers 4, 1, 1, 4 called x
+x<-c(4,1,1,4)
+#2.Create a second vector called y with the numbers 1, 4.
+y<-c(1,4)
+#(c) We have vectors whit different size. The smallar one (y) is recycled and it becomes c(1,4,1,4)
+x-y
+ 
+s<-c(x,y)
+#(e)length=60
+rep(s,10)
+length(rep(s,10))
+#(f)
+rep(s,each=3)
+#(g)
+seq(7,21,by=1)
+7:21
+#(h)
+length(seq(7,21))
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,35 @@
-
+#Problem 2
+#(a)
+xmin<-c(23.0,20.5,28.2,20.3,22.4,17.2,18.2)
+xmax<-c(25.0, 22.8,31.2,27.3,28.4,20.2,24.1)
+#(b)
+xmax-xmin
+#(c)
+avgMAX<-mean(xmax)
+avgMAX
+avgMIN<-mean(xmin)
+avgMIN
+#(d)
+xmin[xmin<avgMIN]
+#(e)
+xmax[xmax>avgMAX]
+#(f)
+names(xmin)<-c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+names(xmax)<-c('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+#(g)
+temperature<-data.frame(xmax,xmin)
+temperature
+#(h)
+temperature<-within(temperature,{xminFahrenheit<-xmin*9/5 +32})
+temperature
+#(i)
+tempFahrenheit<-data.frame(xminFahreneheit=xmin*9/5+32,
+                           xmaxFahreneheit=xmax*9/5+32)
+tempFahrenheit
+#(j)
+d5tempFahrenheit<-data.frame(Min_temp_Fahreneheit=xmin[seq(1:5)]*9/5+32,
+                            Max_temp_Fahrenejeit=xmax[seq(1:5)]*9/5+32)
+d5tempFahrenheit
+d5tempFahrenheit<-data.frame(Min_temp_Fahreneheit=xmin[seq(length(xmin)-2)]*9/5+32,
+                             Max_temp_Fahrenejeit=xmax[seq(length(xmax)-2)]*9/5+32)
+d5tempFahrenheit                                                     
